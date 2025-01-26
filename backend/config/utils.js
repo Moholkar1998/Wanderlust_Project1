17de16c6db724fadc7fa1fcdce58655e90b8069d
Moholkar@1998 <vaibhavmoholkar999@gmail.com>
2025-01-26 11:56:19 +0530

    added gitignore file

--- backend/config/utils.js
+++ backend/config/utils.js
@@ -1,8 +1,11 @@
 import dotenv from 'dotenv';
-dotenv.config();
+// dotenv.config();
+
+dotenv.config({ path: '.env.docker' });
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 const REDIS_URL = process.env.REDIS_URL;
 
+console.log('Mongo URI:', MONGODB_URI);
 export { MONGODB_URI, PORT, REDIS_URL };
